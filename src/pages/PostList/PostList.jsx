8a4985b889dc5a531dab22f4ceb4afcf7c58106a
heejii <heejii97@naver.com>
2023-09-14 20:11:15 +0900

    ADD: 컴포넌트 분리

--- src/pages/PostList/PostList.jsx
+++ src/pages/PostList/PostList.jsx
@@ -1,47 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import List from '../../components/List/List.jsx';
 import './PostList.scss';
+// import '/data/data.json';
 
 const PostList = () => {
+  const [threadList, setThreadList] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/Data.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setThreadList(data);
+      });
+  }, []);
+
+  const navigate = useNavigate();
+
+  const goToEditing = () => {
+    navigate('/post-editing');
+  };
+
   return (
     <div className="postList">
-      <div className="container">
-        <div className="post">
-          <div className="top">
-            <div className="left">
-              <div className="profileImage">
-                <img src="/images/Profile_thumb.png" alt="" />
-              </div>
-              <div className="nickname">
-                <p>이름</p>
-              </div>
-            </div>
-            <div className="right">
-              <div className="date">
-                <p>년.월.일</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="contents">
-            <p>
-              일라이자 효과는 인간의 사고 과정과 감정을 AI 시스템에 잘못 돌리는
-              사람들의 경향을 말하며, 따라서 시스템이 실제보다 더 지능적이라고
-              믿는다. 이 현상은 1966년 MIT 교수 조셉 웨이젠바움이 만든 챗봇인
-              ELIZA의 이름을 따서 명명되었다.
-            </p>
-          </div>
-
-          <div className="comment">
-            <span>댓글</span>
-            <span>0</span>
-          </div>
-        </div>
-
-        <div className="write">
-          <form action="#">
-            <button>글 쓰기</button>
-          </form>
-        </div>
+      {threadList.map(thread => {
+        return <List key={threadList.postId} list={thread} />;
+      })}
+      <div className="write">
+        <button onClick={goToEditing}>글 쓰기</button>
       </div>
     </div>
   );
